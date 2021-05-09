e8517d1644b4bc8e77d6215eedd9f1f555ba2266
ankitbhatia87 <ankit.bhatia87@gmail.com>
2021-05-09 17:03:23 +0530

    add user app done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/User/AddUser";
+import UsersList from "./components/User/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserListHandler = newUser => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, newUser];
+    });
+  }
   return (
     <div>
-
+      <AddUser addUserList={addUserListHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
